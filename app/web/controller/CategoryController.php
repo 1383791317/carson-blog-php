83386d2a2c4e867f88f9e5715c29d937afef1d20
yc-1224 <yc_1224@163.com>
2024-07-05 16:54:46 +0800

    详情

--- app/web/controller/CategoryController.php
+++ app/web/controller/CategoryController.php
@@ -5,6 +5,7 @@ namespace app\web\controller;
 
 use app\BaseController;
 use app\models\article\ArticleCategoryModel;
+use app\models\article\ArticleTagModel;
 use server\ApiReturn;
 use think\Request;
 
@@ -13,6 +14,10 @@ class CategoryController extends BaseController
     public function info(Request $request)
     {
         $id = $request->get('id');
-        return ApiReturn::success(ArticleCategoryModel::query()->findOrEmpty($id));
+        $tagId = $request->get('tag_id');
+        return ApiReturn::success([
+            'category' => $id ? ArticleCategoryModel::query()->field('id,name')->findOrEmpty($id) : null,
+            'tag' => $tagId ? ArticleTagModel::query()->field('id,name')->findOrEmpty($tagId) : null,
+        ]);
     }
 }
